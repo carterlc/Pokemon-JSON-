2be60c26a37fdcfba134d61dba7d9aea7a058e14
LAPTOP-TKJAJ0NE\Carters Laptop <carterlc@live.ca>
2023-01-13 17:09:39 -0600

    Added some template literals that is suposed to display pokemon text information from the JSON file. TODO: Ask Arlin for help as to why the data wont display on the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,25 @@
-import data from './test.json' assert { type: 'json' };
+import data from '../pokedex.json' assert { type: 'json' };
 const output = document.getElementById("output");
 
 let html = "";
-for (let i = 0; i < data.length; i++) {
-    html += `<p>${data[i].name}: ${data[i].value}</p>`;
+for (let i = 0; i < data.pokemon.length; i++) {
+
+console.log(data.pokemon[i].name);
+console.log(data.pokemon[i].height);
+console.log(data.pokemon[i].weight);
+
+html += `
+<div class="card">
+<h1>
+    ${data.pokemon[i].num} - ${data.pokemon[i].name}
+</h1>
+
+<img src="${data.pokemon[i].img}" alt="pokemon image">
+<div class="info">
+    <p>
+        ${data.pokemon[i].type}
+    </p>
+</div>
+</div>`;
 }
 output.innerHTML = html;
