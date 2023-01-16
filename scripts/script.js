fae49fe99bcc73944636e27951ed0ec81c91f97a
LAPTOP-TKJAJ0NE\Carters Laptop <carterlc@live.ca>
2023-01-16 15:04:10 -0600

    Got help from Arlin. Data now prints to the screen thanks to a commenting mistake in the HTML. Also added a math.Random to calculate a random pokemon from the data and presents it on page load.

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,16 @@ import data from '../pokedex.json' assert { type: 'json' };
 const output = document.getElementById("output");
 
 let html = "";
-for (let i = 0; i < data.pokemon.length; i++) {
+let i = Math.floor(Math.random() * data.pokemon.length);
+//TODO make this number random!
+// for (let i = 0; i < data.pokemon.length; i++) {
 
-console.log(data.pokemon[i].name);
-console.log(data.pokemon[i].height);
-console.log(data.pokemon[i].weight);
+    // console.log(data.pokemon[i].name);
+    // document.write(data.pokemon[i].name);
+    // console.log(data.pokemon[i].height);
+    // console.log(data.pokemon[i].weight);
 
-html += `
+    html += `
 <div class="card">
 <h1>
     ${data.pokemon[i].num} - ${data.pokemon[i].name}
@@ -16,10 +19,10 @@ html += `
 
 <img src="${data.pokemon[i].img}" alt="pokemon image">
 <div class="info">
-    <p>
-        ${data.pokemon[i].type}
-    </p>
+<p>
+${data.pokemon[i].type}
+</p>
 </div>
 </div>`;
-}
+// }
 output.innerHTML = html;
