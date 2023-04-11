459b14912e7f0c780a8fe3ad85b4b3b0466fb915
Carter Clifford <carterlc@live.ca>
2023-04-11 01:20:40 -0600

    Updated positioning and changed the positioning of the ::before element to inset.

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,8 @@ const output = document.getElementById("output");
 let html = "";
 //TODO make this number random!
 let i = Math.floor(Math.random() * data.pokemon.length);
-html += `
-<div class="card">
-    <div class = "card-info">
-        <h1>
-        ${data.pokemon[i].num} - ${data.pokemon[i].name}
-        </h1>
-    <div class="img-border">
-        <img class="pokemon" src="${data.pokemon[i].img}" alt="pokemon image">
-        <p class="size">Height:${data.pokemon[i].height} Weight:${data.pokemon[i].weight}</p>
-    </div>  
-    <div class="stats">
-            <p><strong>Type:</strong><span class="right"> ${data.pokemon[i].type}</span></p>
-            <p><strong>Weaknesses:</strong><span class="right">${data.pokemon[i].weaknesses}</span></p>
-            <p><strong>Spawn chance:</strong> <span class="right">${data.pokemon[i].spawn_chance}</span></p>
-        </div>
-    </div>
-</div>`
+
+
 
 
 let TotalHeight = 0;
@@ -36,13 +21,13 @@ for (let i = 0; i < data.pokemon.length; i++) {
     //Add up the total damage to calculate the average damage across all pokemon
     //replace meters from the json string
     let withoutm = 0;
-    withoutm = data.pokemon[i].height.replace("m","");
+    withoutm = data.pokemon[i].height.replace("m", "");
     withoutm = withoutm.trim();
     TotalHeight += parseFloat(withoutm);
 
     //replace kg from the json string data
     let withoutkg;
-    withoutkg = data.pokemon[i].weight.replace("kg","");
+    withoutkg = data.pokemon[i].weight.replace("kg", "");
     withoutkg = withoutkg.trim();
     TotalWeight += parseFloat(withoutkg);
     //spawns is whole int so no conversions neccessary
@@ -58,14 +43,33 @@ AvgSpawns = TotalSpawns / data.pokemon.length;
 AvgSpawns = Math.round(AvgSpawns);
 
 html += `
-<div class = "fact-border">
-    <div class = "facts">
-        <h1>Pokemon Facts</h1>
-        <p><strong>Average height of all pokemon:</strong><span class = "right">${AvgHeight} m</span></p>
-        <p><strong>Average weight of all pokemon:</strong><span class = "right">${AvgWeight} kg</span></p>
-        <p><strong>Average spawns of all pokemon:</strong><span class = "right">${AvgSpawns}</span></p>
+<div class="container">
+    <div class="card">
+        <div class = "card-info">
+            <h1>
+            ${data.pokemon[i].num} - ${data.pokemon[i].name}
+            </h1>
+        <div class="img-border">
+            <img class="pokemon" src="${data.pokemon[i].img}" alt="pokemon image">
+            <p class="size">Height:${data.pokemon[i].height} Weight:${data.pokemon[i].weight}</p>
+        </div>  
+        <div class="stats">
+                <p><strong>Type:</strong><span class="right"> ${data.pokemon[i].type}</span></p>
+                <p><strong>Weaknesses:</strong><span class="right">${data.pokemon[i].weaknesses}</span></p>
+                <p><strong>Spawn chance:</strong> <span class="right">${data.pokemon[i].spawn_chance}</span></p>
+            </div>
+        </div>
+    </div>
+    <div class = "fact-border">
+        <div class = "facts">
+            <h1>Pokemon Facts</h1>
+            <p><strong>Average height of all pokemon:</strong><span class = "right">${AvgHeight} m</span></p>
+            <p><strong>Average weight of all pokemon:</strong><span class = "right">${AvgWeight} kg</span></p>
+            <p><strong>Average spawns of all pokemon:</strong><span class = "right">${AvgSpawns}</span></p>
+        </div>
     </div>
 </div>`;
+
 
 
 output.innerHTML = html;
